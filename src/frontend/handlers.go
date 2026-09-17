9be988924fd955f3656dd3ef57c5cc76d4de9fbf
Edekiagbi <abrahamagbi149@gmail.com>
2026-09-18 00:05:34 +0100

    update

--- src/frontend/handlers.go
+++ src/frontend/handlers.go
@@ -401,6 +401,12 @@ func (fe *frontendServer) placeOrderHandler(w http.ResponseWriter, r *http.Reque
 }
 
 func (fe *frontendServer) assistantHandler(w http.ResponseWriter, r *http.Request) {
+	log := r.Context().Value(ctxKeyLog{}).(logrus.FieldLogger)
+	if !assistantEnabled || fe.shoppingAssistantSvcAddr == "" {
+		renderHTTPError(log, r, w, errors.New("assistant is not enabled"), http.StatusNotFound)
+		return
+	}
+
 	currencies, err := fe.getCurrencies(r.Context())
 	if err != nil {
 		renderHTTPError(log, r, w, errors.Wrap(err, "could not retrieve currencies"), http.StatusInternalServerError)
@@ -450,6 +456,11 @@ func (fe *frontendServer) getProductByID(w http.ResponseWriter, r *http.Request)
 
 func (fe *frontendServer) chatBotHandler(w http.ResponseWriter, r *http.Request) {
 	log := r.Context().Value(ctxKeyLog{}).(logrus.FieldLogger)
+	if !assistantEnabled || fe.shoppingAssistantSvcAddr == "" {
+		renderHTTPError(log, r, w, errors.New("assistant is not enabled"), http.StatusNotFound)
+		return
+	}
+
 	type Response struct {
 		Message string `json:"message"`
 	}
